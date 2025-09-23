6625fcd84411fe03fa320a2ef7844aff7a5dfef2
Сысоев Давид <sysoye@inbox.ru>
2025-09-23 19:59:41 +0300

    Laravel курс с нуля, база. 29. Пагинация в Laravel

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,13 +1,15 @@
 @extends("layouts.main")
 @section('content')
-    <div>
+    <div class="mt-3">
         <a href="{{route('post.create')}}" class="btn btn-primary mb-3">Create</a>
     </div>
-    <div class="">
-        @foreach ($posts as $post)
-            <a href="{{route('post.show', $post->id)}}">
-                <div>{{$post->id}}. {{$post->title}}</div>
-            </a>
-        @endforeach
+    @foreach ($posts as $post)
+        <a href="{{route('post.show', $post->id)}}">
+            <div>{{$post->id}}. {{$post->title}}</div>
+        </a>
+    @endforeach
+    <div class="mt-3">
+        {{$posts->links()}}
     </div>
+
 @endsection
